deba4684781ac64748c4b1d240d31a12c84c94ee
meto00 <metinvatansevernitem@gmail.com>
2016-03-21 19:48:29 +0200

    Added files via upload
    Terminal Sanal Sunucumuz herşey buna bağlancak

---
+++ Terminal/Terminal/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Terminal
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            TcpListener serversocket = new TcpListener(8080);
+            int requestcount = 0;
+            TcpClient clientsocket = default(TcpClient);
+            serversocket.Start();
+            Console.WriteLine(">> Server Started");
+            clientsocket = serversocket.AcceptTcpClient();
+            Console.WriteLine(">> Accept Connection from Client");
+            requestcount = 0;
+            while((true))
+            {
+                try
+                {
+                    requestcount += 1;
+                    NetworkStream networkstream = clientsocket.GetStream();
+                    byte[] bytesFrom = new byte[100025];
+                    networkstream.Read(bytesFrom, 0, (int)clientsocket.ReceiveBufferSize);
+                    string dataFC = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    Console.WriteLine(">> Data From Client - " + dataFC);
+                    string serverresponse = "Last Message From Client" + dataFC;
+                    byte[] sendBytes = Encoding.ASCII.GetBytes(serverresponse);
+                    networkstream.Write(sendBytes, 0, sendBytes.Length);
+                    networkstream.Flush();
+                    Console.WriteLine(" >> "+serverresponse);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            clientsocket.Close();
+            serversocket.Stop();
+            Console.WriteLine(" >> Exit ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Terminal/Terminal/Receiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace Terminal
+{
+    class Receiver
+    {
+        public void WriteXml(byte[] buffer)
+        {
+            
+        }
+    }
+}
